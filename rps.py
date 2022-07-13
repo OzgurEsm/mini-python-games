a455f738888d85a82e3dbd75f88522599fdb7eab
OzgurEsm <90386838+OzgurEsm@users.noreply.github.com>
2022-07-13 10:58:09 +0300

    Update rps.py

--- rps.py
+++ rps.py
@@ -20,7 +20,7 @@ def rock_paper_scissors():
     
     if user_input == computer_input:
         print("It's a tie!, Choose again!")
-        return
+        return rock_paper_scissors()
     elif user_input == r and computer_input == s:
         print("Rock smashes scissors!: You won!") 
     elif user_input == s and computer_input == p:
